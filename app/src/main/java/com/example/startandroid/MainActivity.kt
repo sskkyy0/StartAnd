643602335986f76105494d0d91c671ae536b4962
sskkyy0 <zxzxzxzxzxzxzz@naver.com>
2025-03-03 17:24:40 +0900

    refactor: MainActivity로 다시 전환

--- app/src/main/java/com/example/startandroid/MainActivity.kt
+++ app/src/main/java/com/example/startandroid/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.startandroid
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.startandroid.adddelete.AddDeleteScreen
 import com.example.startandroid.profilecard.ProfileCardScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ProfileCardScreen()
+            //ProfileCardScreen()
+            AddDeleteScreen()
         }
     }
 }
--- app/src/main/java/com/example/startandroid/adddelete/AddDelete.kt
+++ app/src/main/java/com/example/startandroid/adddelete/AddDelete.kt
@@ -30,14 +30,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
-class AddDelete: ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent{
-            AddDeleteScreen()
-        }
-    }
-}
 
 @Composable
 fun AddDeleteScreen(){
